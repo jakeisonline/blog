0b3be2473db7078c73027ca213556415ada7150e
Jake Holman <me@jakeisonline.com>
2025-03-29 20:49:07 +0000

    add visual language example

--- src/content/examples/index.tsx
+++ src/content/examples/index.tsx
@@ -6,41 +6,35 @@ export const Index: Record<string, any> = {
     component: React.lazy(
       () => import("@/content/examples/pending-state/no-feedback"),
     ),
-    source: (
-      await import(`@/content/examples/pending-state/no-feedback.tsx?raw`)
-    ).default,
   },
   "pending-state-with-feedback": {
     name: "pending-state-with-feedback",
     component: React.lazy(
       () => import("@/content/examples/pending-state/with-feedback"),
     ),
-    source: (
-      await import(`@/content/examples/pending-state/with-feedback.tsx?raw`)
-    ).default,
   },
   "pending-state-with-context": {
     name: "pending-state-with-context",
     component: React.lazy(
       () => import("@/content/examples/pending-state/with-context"),
     ),
-    source: (
-      await import(`@/content/examples/pending-state/with-context.tsx?raw`)
-    ).default,
   },
   "pending-state-apply-filters": {
     name: "pending-state-apply-filters",
     component: React.lazy(
       () => import("@/content/examples/pending-state/apply-filters"),
     ),
-    source: (
-      await import(`@/content/examples/pending-state/apply-filters.tsx?raw`)
-    ).default,
   },
   "pending-state-navigation": {
     name: "pending-state-navigation",
     component: React.lazy(
       () => import("@/content/examples/pending-state/navigation"),
+    ),
+  },
+  "visual-language-food-icons": {
+    name: "visual-language-food-icons",
+    component: React.lazy(
+      () => import("@/content/examples/visual-language/food-icons"),
     ),
   },
 }
